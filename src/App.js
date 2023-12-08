c68ab98dfe7713b4bfb7397da09db656de678e1c
anurag-ay <ay1909069@gmail.com>
2023-12-08 22:04:21 +0530

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PersonalityForm from "./pages/PersonalityForm";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<PersonalityForm />}
+          // element={token ? ChatWithContext : <Navigate to="/login" />}
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
