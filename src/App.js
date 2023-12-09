194a1346725f84eeee4664c0e7a12e46d3c9128b
anurag-ay <ay1909069@gmail.com>
2023-12-09 22:51:05 +0530

    update app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PersonalityForm from "./pages/PersonalityForm";
 import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import LogIn from "./pages/Login";
+import SignUp from "./pages/Signup";
+import { useEffect, useState } from "react";
+import UserInfoProvider from "./context/userInfoContex";
 
 function App() {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    let tokenValue = localStorage.getItem("token");
+    setToken(tokenValue);
+  }, [token]);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route
           path="/"
-          element={<PersonalityForm />}
-          // element={token ? ChatWithContext : <Navigate to="/login" />}
+          element={
+            token ? (
+              <UserInfoProvider>
+                <Home />
+              </UserInfoProvider>
+            ) : (
+              <LogIn />
+            )
+          }
         />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
